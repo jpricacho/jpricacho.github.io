9c7c97e0b6bcd430dcade2f14ae9842e45931ee5
jpricacho <jpricacho27@gmail.com>
2022-04-03 12:36:16 +0800

    ecalc

--- ecalc/js/calc.js
+++ ecalc/js/calc.js
@@ -4,6 +4,7 @@ const cancelButton = document.querySelectorAll(".cancelButton");
 const eqButton = document.querySelector(".eqButton");
 const result = document.querySelector("#result");
 const result2 = document.querySelector("#result2");
+const calculator = document.querySelector(".calculator")
 let joinedResult1 = [];
 let joinedResult2 = "";
 let oprToUse;
@@ -12,6 +13,47 @@ let num1;
 let num2;
 let num3;
 
+document.onkeyup = getKey;
+
+function getKey(e) {
+    if (e.keyCode == 96) {
+        document.querySelector("#num0").click();
+    } else if (e.keyCode == 97) {
+        document.querySelector("#num1").click();
+    } else if (e.keyCode == 98) {
+        document.querySelector("#num2").click();
+    } else if (e.keyCode == 99) {
+        document.querySelector("#num3").click();
+    } else if (e.keyCode == 100) {
+        document.querySelector("#num4").click();
+    } else if (e.keyCode == 101) {
+        document.querySelector("#num5").click();
+    } else if (e.keyCode == 102) {
+        document.querySelector("#num6").click();
+    } else if (e.keyCode == 103) {
+        document.querySelector("#num7").click();
+    } else if (e.keyCode == 104) {
+        document.querySelector("#num8").click();
+    } else if (e.keyCode == 105) {
+        document.querySelector("#num9").click();
+    } else if (e.keyCode == 110) {
+        document.querySelector("#numDot").click();
+    } else if (e.keyCode == 109) {
+        document.querySelector("#opr1").click();
+    } else if (e.keyCode == 107) {
+        document.querySelector("#opr2").click();
+    } else if (e.keyCode == 106) {
+        document.querySelector("#opr3").click();
+    } else if (e.keyCode == 111) {
+        document.querySelector("#opr4").click();
+    } else if (e.keyCode == 13) {
+        document.querySelector("#eq").click();
+    } else if (e.keyCode == 8) {
+        document.querySelector("#c1").click();
+    } else if (e.keyCode == 46) {
+        document.querySelector("#c2").click();
+    }
+}
 
 numButton.forEach((evt) => {
     evt.addEventListener("click", getVal, false);
